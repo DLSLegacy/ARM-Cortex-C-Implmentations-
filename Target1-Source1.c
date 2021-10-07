9e5b562997cf6fd3bf21a7002477d0b14aa253ea
Josh <j05hua13451@gmail.com>
2021-10-07 15:17:59 -0400

    Initialization File

---
+++ Target1-Source1.c
@@ -0,0 +1,17 @@
+#include <LPC17xx.H> // Initialization program For Cortex M3 
+int main() 
+{  
+    int a=0x10; 
+    int b=0x20; 
+    int c; 
+
+    c=a+b 
+
+} 
+
+/* Watch 1 
+        .     Value         Type
+a          0x100000010     int     
+b          0x100000020     int
+c          0x100000030     int 
+*/
